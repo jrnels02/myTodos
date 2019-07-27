0a046cfac05b1e60035d0865c248e1ee821571d4
Jeffrey Nelson Jr <neljstytretja@gmail.com>
2019-07-26 18:02:59 -0600

    Initial Commit

---
+++ myTodos WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  myTodos WatchKit Extension
+//
+//  Created by Jrnels02 on 7/26/19.
+//  Copyright © 2019 Jrnels02. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ myTodos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTodos
+//
+//  Created by Jrnels02 on 7/26/19.
+//  Copyright © 2019 Jrnels02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
